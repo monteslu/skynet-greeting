90c7160003440e2d7762e15a66102988e00d5d20
Luis Montes <luis@iceddev.com>
2014-03-09 07:05:21 -0700

    update for message.payload change

--- index.js
+++ index.js
@@ -7,29 +7,30 @@ function Plugin(messenger, options){
 }
 
 var optionsSchema = {
-  'type': 'object',
-  'properties': {
-    'greetingPrefix': {
-      'type': 'string',
-      'required': true
+  type: 'object',
+  properties: {
+    greetingPrefix: {
+      type: 'string',
+      required: true
     }
   }
 };
 
 var messageSchema = {
-  'type': 'object',
-  'properties': {
-    'text': {
-      'type': 'string',
-      'required': true
+  type: 'object',
+  properties: {
+    text: {
+      type: 'string',
+      required: true
     }
   }
 };
 
-Plugin.prototype.onMessage = function(data){
-  console.log(this.options.greetingPrefix + ', ' + data.fromUuid);
-  if(data.fromUuid){
-    this.messenger.send({devices: data.fromUuid, message: {greeting: this.options.greetingPrefix + ' back atcha: ' + data.message.text}});
+Plugin.prototype.onMessage = function(message){
+  var data = message.message || message.payload;
+  console.log(this.options.greetingPrefix + ', ' + message.fromUuid);
+  if(message.fromUuid){
+    this.messenger.send({devices: message.fromUuid, message: {greeting: this.options.greetingPrefix + ' back atcha: ' + data.text}});
   }
 
 };
